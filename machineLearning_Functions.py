7640c8640f27a206843c85cf070a58c336078d9a
rthornton3i <43607226+rthornton3i@users.noreply.github.com>
2019-02-18 17:24:04 -0500

    Add files via upload

--- machineLearning_Functions.py
+++ machineLearning_Functions.py
@@ -2,36 +2,75 @@ import numpy as np
 
 import miscellaneous_Functions as misc
 
-def networkSetup(inputs,numHLs,numOuts,minLim=0,maxLim=1):
-    inputs = np.asarray(inputs)
-    ht,wd = np.shape(inputs)
+def layerSetup(inputs,numOutputs,symOpt,curvOpt):
+    inputs = np.reshape(inputs,(np.shape(inputs)[1],1)) if np.shape(inputs)[0] == 1 else inputs
+    inputs = misc.linNorm(inputs)   
+    numInputs = np.shape(inputs)[0]
     
-    inputs = np.reshape(inputs,(wd,1)) if ht == 1 else inputs
-    ht,wd = np.shape(inputs)
+    wts = misc.randArray((numOutputs,numInputs),sym=symOpt)
+    bias = misc.randArray((numOutputs,1),sym=symOpt)
     
-    inputs = misc.linNorm(inputs,minLim,maxLim)
+    outputs = np.dot(wts,inputs).reshape(numOutputs,1) + bias
+    outputs = misc.sigNorm(outputs,curveType=curvOpt)
     
-    allWts = []
-    allNodes = []
-    numLoops = len(numHLs)+1
-    for n in range(numLoops):
-        if n == 0:
-            wts = np.random.rand(numHLs[n],ht)
-            nodes = misc.sigNorm(np.dot(wts,inputs))
-        elif n == numLoops-1:
-            wts = np.random.rand(numOuts,numHLs[n-1])
-            nodes = misc.sigNorm(np.dot(wts,allNodes[n-1]))
-        else:
-            wts = np.random.rand(numHLs[n],numHLs[n-1])
-            nodes = misc.sigNorm(np.dot(wts,allNodes[n-1]))
+    return [inputs,wts,bias,outputs]
+
+def networkSetup(inputs,numNodes,symOpt=False,curvOpt='logistic'):
+    numLayers = len(numNodes)
+    networkNodes = []
+    networkWts = []
+    networkBias = []
+    
+    for n in range(numLayers):
+        layerInputs = inputs
+            
+        [layerInputs,layerWts,layerBias,layerOutputs] = layerSetup(layerInputs,numNodes[n],symOpt,curvOpt)
+        networkNodes.append(layerInputs)
+        networkWts.append(layerWts)
+        networkBias.append(layerBias)
         
-        allWts.append(wts)
-        allNodes.append(nodes)
-        
-    outputs = allNodes[-1]
-    del(allNodes[-1])
-        
-    return [inputs,outputs,allWts,allNodes]
+        inputs = layerOutputs
+        networkOutputs = layerOutputs
+
+    return [networkNodes,networkWts,networkBias,networkOutputs]
+
+inputs = [2,5,9,10]
+numNodes = [8,6,2]
+
+[nodes,wts,bias,outputs] = networkSetup(inputs,numNodes,symOpt=True,curvOpt='hyperbolic')
+
+print(outputs)
+
+#def networkSetup(inputs,numHLs,numOuts,minLim=0,maxLim=1):
+#    inputs = np.asarray(inputs)
+#    ht,wd = np.shape(inputs)
+#    
+#    inputs = np.reshape(inputs,(wd,1)) if ht == 1 else inputs
+#    ht,wd = np.shape(inputs)
+#    
+#    inputs = misc.linNorm(inputs,minLim,maxLim)
+#    
+#    allWts = []
+#    allNodes = []
+#    numLoops = len(numHLs)+1
+#    for n in range(numLoops):
+#        if n == 0:
+#            wts = np.random.rand(numHLs[n],ht)
+#            nodes = misc.sigNorm(np.dot(wts,inputs))
+#        elif n == numLoops-1:
+#            wts = np.random.rand(numOuts,numHLs[n-1])
+#            nodes = misc.sigNorm(np.dot(wts,allNodes[n-1]))
+#        else:
+#            wts = np.random.rand(numHLs[n],numHLs[n-1])
+#            nodes = misc.sigNorm(np.dot(wts,allNodes[n-1]))
+#        
+#        allWts.append(wts)
+#        allNodes.append(nodes)
+#        
+#    outputs = allNodes[-1]
+#    del(allNodes[-1])
+#        
+#    return [inputs,outputs,allWts,allNodes]
 
 def networkTrain(inputs,allWts,allNodes):
     
--- miscellaneous_Functions.py
+++ miscellaneous_Functions.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def linNorm(data,minLim,maxLim):
+def linNorm(data,minLim=0,maxLim=1):
     dataMax = np.max(data)
     dataMin = np.min(data)
     
@@ -8,7 +9,25 @@ def linNorm(data,minLim,maxLim):
     
     return normData
 
-def sigNorm(data):
-    normData = 1 / (1 + np.exp(-data))
+def sigNorm(data,curveType='logistic',plotOpt=False):
+    if curveType == 'logistic':
+        normData = 1 / (1 + np.exp(-data))
+    elif curveType == 'hyperbolic':
+        normData = np.tanh(data)
+    elif curveType == 'arctan':
+        normData = np.arctan(data)
+    elif curveType == 'sqrt':
+        normData = data / np.sqrt(1 + data ** 2)
+    else:
+        raise Exception('ERROR: Invalid curve type specified.')
     
     return normData
+    
+data = np.arange(10)
+test = sigNorm(data,plotOpt=True)
+    
+def randArray(dims,sym=False):
+    randNums = np.random.random(size=dims)
+    randNums = randNums * 2 - 1 if sym is True else randNums
+    
+    return randNums
